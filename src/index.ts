bee2fd37c887ba72eac171340e3debb4b387a2ae
yoshiioko <acardoza@pdx.edu>
2022-12-20 22:31:44 -0800

    add import for metaplex. create Metaplex instance.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,45 @@
-import { initializeKeypair } from "./initializeKeypair"
-import * as web3 from "@solana/web3.js"
+import { initializeKeypair } from "./initializeKeypair";
+import * as web3 from "@solana/web3.js";
+import { Connection, clusterApiUrl, PublicKey } from "@solana/web3.js";
+import {
+  Metaplex,
+  keypairIdentity,
+  bundlrStorage,
+  toMetaplexFile,
+  NftWithToken,
+} from "@metaplex-foundation/js";
+import * as fs from "fs";
+
+const tokenName = "Token Name";
+const description = "Description";
+const symbol = "SYMBOL";
+const sellerFeeBasisPoints = 100;
+const imageFile = "test.png";
 
 async function main() {
-  const connection = new web3.Connection(web3.clusterApiUrl("devnet"))
-  const user = await initializeKeypair(connection)
+  const connection = new web3.Connection(web3.clusterApiUrl("devnet"));
+  const user = await initializeKeypair(connection);
 
-  console.log("PublicKey:", user.publicKey.toBase58())
+  console.log("PublicKey:", user.publicKey.toBase58());
+
+  // 1. Create a Metaplex instance
+  const metaplex = Metaplex.make(connection)
+    .use(keypairIdentity(user))
+    .use(
+      bundlrStorage({
+        address: "https://devnet.bundlr.network",
+        providerUrl: "https://api.devnet.solana.com",
+        timeout: 60000,
+      })
+    );
 }
 
 main()
   .then(() => {
-    console.log("Finished successfully")
-    process.exit(0)
+    console.log("Finished successfully");
+    process.exit(0);
   })
   .catch((error) => {
-    console.log(error)
-    process.exit(1)
-  })
+    console.log(error);
+    process.exit(1);
+  });
